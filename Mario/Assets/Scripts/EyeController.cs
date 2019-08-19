c01fdb8aec8273f939fa121214fec341f109004d
ldihl <leandro.dihl@gmail.com>
2019-08-19 17:36:30 -0300

    eyes1

---
+++ Mario/Assets/Scripts/EyeController.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeController : MonoBehaviour
+{
+    //blinking time (frames) and steps (how much the eye close/open when blink)
+    public int blinkTime = 5;
+    public float blinkStep = 0.1f;
+
+    //while it is blinking
+    public bool isBlinking = false;
+    public int blinkWait;
+    public bool closing = true;
+
+    //default transform rotation of the eye color
+    public Quaternion defaultRotation;
+
+    public GameObject col;
+        
+    // Start is called before the first frame update
+    void Awake()
+    {
+        blinkWait = blinkTime;
+        defaultRotation = transform.GetChild(0).transform.rotation;
+    }
+
+    // Update is called once per frame
+    //Human blinks around a wink each 4 seconds.
+    // At 50FPS, a blink each 200 frames.
+    //Also, takes around 0.1 seconds
+    void Update()
+    {
+        if(Time.frameCount % 200 == 0)
+        {
+            isBlinking = true;
+        }
+
+        if (isBlinking)
+        {
+            Blink();
+        }
+
+        //follow mouse
+        //FollowMouse();
+    }
+
+    //eye follows the mouse
+    public void FollowMouse()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //mouseRay = new Ray(new Vector3(mouseRay.origin.x, mouseRay.origin.y, 0), 
+        //new Vector3(mouseRay.direction.x, mouseRay.direction.y, 0.1f));
+        float midPoint = (transform.GetChild(0).transform.position - Camera.main.transform.position).magnitude * 0.5f;
+        Vector3 lookThere = mouseRay.origin + mouseRay.direction * midPoint;
+        transform.GetChild(0).transform.LookAt(lookThere);
+    }
+
+    //saccade behavior
+    public void SaccadeBehavior(float saccadeMag)
+    {
+        //saccade
+        transform.GetChild(0).transform.rotation = defaultRotation;
+        transform.GetChild(0).transform.Rotate(new Vector3(0, 0, saccadeMag/3.14f));
+    }
+
+    //bliiink mothafucka!
+    public void Blink()
+    {
+        //if it is closing, eye diminish
+        if (closing)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - blinkStep, 
+                transform.localScale.z);
+        }
+            
+        //if the eye is closed, wait a bit
+        if (transform.localScale.y <= 0 && blinkWait > 0)
+        {
+            closing = false;
+            blinkWait--;
+        }
+
+        //if it is not closing, eye open
+        if (!closing)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + blinkStep,
+                transform.localScale.z);
+        }
+
+        //donezzo
+        if(blinkWait < blinkTime && transform.localScale.y >= 1.5f)
+        {
+            isBlinking = false;
+            closing = true;
+            blinkWait = blinkTime;
+        }
+    }
+
+    public void Movement(List<string> l, int side)
+    {
+        int index;
+        string[] coord = l.ToArray();
+        float[] cx = new float[7];
+        float[] cy = new float[7];
+
+        for (index = 0; index < 7; index++)
+        {
+            cx[index] = float.Parse(coord[index].Split(',')[0]);
+            cy[index] = float.Parse(coord[index].Split(',')[1]);
+        }
+
+        Vector3[] worldC = new Vector3[6];
+
+        /*worldC[0] = transform.Find("A").transform.localPosition;
+        worldC[1] = transform.Find("B").transform.localPosition;
+        worldC[2] = transform.Find("C").transform.localPosition;
+        worldC[3] = transform.Find("D").transform.localPosition;
+        worldC[4] = transform.Find("E").transform.localPosition;
+        worldC[5] = transform.Find("F").transform.localPosition;*/
+
+        //Debug.Log("42: "+cx[6]+" , "+cy[6]+" ; 45: "+cx[9]+" , "+cy[9]+" ; 69: "+cx[13]+" , "+cy[13]);
+
+        float propImg;
+        float propWorld;
+        if (side == 1)
+        {
+            /*propImg = (cx[6] - cx[3]) / (cx[3] - cx[0]);
+            propWorld = propImg * (10 - 20) + 20;*/
+            propWorld = (315 * cx[0] - 225 * cx[3] - 90 * cx[6]) / (cx[3] - cx[0]);
+            //Debug.Log("Right: pImg: "+propImg+" pWorld: "+propWorld);
+        }
+        else
+        {
+            //propImg = (cx[6] - cx[3]) / (cx[0] - cx[3]);
+            //propWorld = cx[0]
+            propWorld = (315*cx[0]-225*cx[3]-90*cx[6]) / (cx[3] - cx[0]);
+            
+         //   Debug.Log("Left: pImg: " + propImg );
+        }
+
+        //float propImg = (cx[6] - cx[0]) / (cx[3] - cx[0]);
+        //float propWorld = propImg * (10 - 35) + 35;
+        //Debug.Log(propWorld);
+
+        /*Vector3 relativePos = (new Vector3(propWorld, 0, 0)) - col.transform.position;
+
+        // the second argument, upwards, defaults to Vector3.up
+        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        col.transform.rotation = rotation;
+        */
+
+        col.transform.rotation = defaultRotation;
+        col.transform.Rotate(new Vector3(0, propWorld * Time.deltaTime * 0.1f, 0));
+        /*if (propWorld >= 0.5f)
+        {
+            Debug.Log(propWorld + " >>>>>>>>> 0.5 ");
+            col.transform.Rotate(new Vector3(0, -propWorld / 55, 0));
+        }
+        else
+        {
+            col.transform.Rotate(new Vector3(0, propWorld / 55, 0));
+            Debug.Log(" <<<<<<<<< 0.5 ");
+        }*/
+
+        //Debug.Log(transform.name + "NOT NORMALIZED-----" + new Vector3(0, propWorld, 0));
+        //transform.GetChild(0).transform
+
+        //Debug.Log(worldC[0].x);
+
+
+        /*float cx = (p42x + p45x)/2;
+        float cy = (p42y + p45y)/2;
+        float vx = cx - p69x; 
+        float vy = cy - p69y;*/
+
+        /*Vector2 norm = new Vector2(vx, vy).normalized;
+        Vector3 v = new Vector3(Mathf.Asin(norm.y), Mathf.Sin(norm.x), 0f);
+        transform.GetChild(0).localEulerAngles = v;
+        Debug.Log(cx + " " + cy + " " + vx + " " + vy+ " " + norm + " " + v);*/
+    }
+}
---
+++ Mario/Assets/Scripts/MainController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    private GameObject[] eyes;
+    private string[] arr;
+    private int k, index;
+    public GameObject left, right;
+
+    private void Awake()
+    {
+        eyes = GameObject.FindGameObjectsWithTag("Eye");
+
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<string> lst = new List<string>();
+        foreach (string file in System.IO.Directory.EnumerateFiles("Assets/Data/angry_out_txt", "*.txt"))
+        {
+            string[] lines = System.IO.File.ReadAllLines(file);
+            // Left eye
+            lst.Add(lines[35]); lst.Add(lines[36]); lst.Add(lines[37]);
+            lst.Add(lines[38]); lst.Add(lines[39]); lst.Add(lines[40]);
+            // Right eye
+            lst.Add(lines[41]); lst.Add(lines[42]); lst.Add(lines[43]);
+            lst.Add(lines[44]); lst.Add(lines[45]); lst.Add(lines[46]);
+            // Pupils
+            lst.Add(lines[67]); lst.Add(lines[68]);      
+        }
+        arr = lst.ToArray();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            string[] str = arr[i].Split(':');
+            arr[i] = str[1];
+            string[] aux = arr[i].Split(',');
+            arr[i] = aux[0] + "," + aux[1];
+        }
+
+        
+        index = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for each eye, generate a saccade behavior
+        //float saccadeMag = CalculateSaccade();
+
+        //foreach(GameObject eye in eyes)
+        //{
+        //eye.GetComponent<EyeController>().SaccadeBehavior(saccadeMag);
+        //Debug.Log(arr.Length);
+
+        if (index < arr.Length - 2 * 14)
+        {
+            arr[index + 12] = ((float.Parse(arr[index + 12].Split(',')[0]) + float.Parse(arr[index + 12 + 14].Split(',')[0]) + float.Parse(arr[index + 12 + 2 * 14].Split(',')[0])) / 3).ToString("0.0000")
+                              + ","
+                              + ((float.Parse(arr[index + 12].Split(',')[1]) + float.Parse(arr[index + 12 + 14].Split(',')[1]) + float.Parse(arr[index + 12 + 2 * 14].Split(',')[1])) / 3).ToString("0.0000");
+
+            arr[index + 13] = ((float.Parse(arr[index + 13].Split(',')[0]) + float.Parse(arr[index + 13 + 14].Split(',')[0]) + float.Parse(arr[index + 13 + 2 * 14].Split(',')[0])) / 3).ToString("0.0000")
+                              + ","
+                              + ((float.Parse(arr[index + 13].Split(',')[1]) + float.Parse(arr[index + 13 + 14].Split(',')[1]) + float.Parse(arr[index + 13 + 2 * 14].Split(',')[1])) / 3).ToString("0.0000");
+        }
+
+        if (index < arr.Length)
+        {
+            List<string> l = new List<string>();
+            List<string> m = new List<string>();
+            for (k = 0; k < 6; k++)
+                l.Add(arr[k+index]);
+            l.Add(arr[12+index]);
+            for (k = 6; k < 12; k++)
+                m.Add(arr[k + index]);
+            m.Add(arr[13 + index]);
+            right.GetComponent<EyeController>().Movement(m, 1);
+            left.GetComponent<EyeController>().Movement(l, 0);
+            index += 14;
+        }
+        else
+        {
+            index = 0;
+        }
+        //}
+    }
+
+    private float CalculateSaccade()
+    {
+        return (-6.9f * Mathf.Log(Random.Range(1, 15) / 15.7f));
+    }
+}
